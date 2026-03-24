143fc026f8aac760f0945fc6ce5d57f2589429a7
sravanthi-aavula <cloudshell-user@ip-10-131-7-90.eu-north-1.compute.internal>
2026-03-24 11:21:50 +0000

    Changed kinux deployment script

--- linux/deploy_linux.sh
+++ linux/deploy_linux.sh
@@ -12,7 +12,7 @@ aws ec2 run-instances \
 --image-id ami-037688ecd92e8611e \
 --instance-type t3.micro \
 --count 1 \
---user-data file://linux/linux1.sh \
+--user-data file://linux1.sh \
 --tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Linux-Nginx}]' 
  
 #############################################
@@ -25,7 +25,7 @@ aws ec2 run-instances \
 --image-id ami-037688ecd92e8611e \
 --instance-type t3.micro \
 --count 1 \
---user-data file://linux/linux2.sh \
+--user-data file://linux2.sh \
 --tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Linux-Docker}]'
 
  
@@ -40,7 +40,7 @@ aws ec2 run-instances \
 --image-id ami-037688ecd92e8611e \
 --instance-type t3.micro \
 --count 1 \
---user-data file://linux/linux3.sh \
+--user-data file://linux3.sh \
 --tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Linux-Jenkins}]'
 
 
